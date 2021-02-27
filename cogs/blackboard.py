895f68e8d924ca3c082f8ac01986d6a95c0c3acc
Marzuq Quraishi <73802175+MarzuqQ@users.noreply.github.com>
2021-02-27 16:11:37 -0600

    Method Headers

--- cogs/blackboard.py
+++ cogs/blackboard.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 
-
 class BlackBoard(commands.Cog):
 
     def __init__(self, client):
@@ -10,6 +9,45 @@ class BlackBoard(commands.Cog):
     @commands.command()
     async def addQuestion(self, ctx):
         pass
+
+    @commands.command()
+    def removeQuestion(self, ctx):
+        pass
+
+    @commands.command()
+    def points(self, ctx):
+        pass
+
+    @commands.command()
+    def startGame(self, ctx):
+        pass
+
+    @commands.command()
+    def endGame(self, ctx):
+        pass
+
+    @commands.command()
+    def join(self, ctx):
+        pass
+
+    @commands.command()
+    def leave(self, ctx):
+        pass
+
+    @commands.command()
+    def printQuestion(self, ctx):
+        pass
+
+    @commands.command()
+    def printAnswer(self, ctx):
+        pass
+
+    @commands.command()
+    def getData(self, ctx):
+        pass
+
+
+
 
 def setup(client):
     client.add_cog(BlackBoard(client))

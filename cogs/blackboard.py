11e9cace2b9d0f5f3bf1b15bd2193d35920add69
Armaan <armaandesayuno@gmail.com>
2021-02-27 16:14:28 -0600

    Fixed method headers

--- cogs/blackboard.py
+++ cogs/blackboard.py
@@ -11,39 +11,39 @@ class BlackBoard(commands.Cog):
         pass
 
     @commands.command()
-    def removeQuestion(self, ctx):
+    async def removeQuestion(self, ctx):
         pass
 
     @commands.command()
-    def points(self, ctx):
+    async def points(self, ctx):
         pass
 
     @commands.command()
-    def startGame(self, ctx):
+    async def startGame(self, ctx):
         pass
 
     @commands.command()
-    def endGame(self, ctx):
+    async def endGame(self, ctx):
         pass
 
     @commands.command()
-    def join(self, ctx):
+    async def join(self, ctx):
         pass
 
     @commands.command()
-    def leave(self, ctx):
+    async def leave(self, ctx):
         pass
 
     @commands.command()
-    def printQuestion(self, ctx):
+    async def printQuestion(self, ctx):
         pass
 
     @commands.command()
-    def printAnswer(self, ctx):
+    async def printAnswer(self, ctx):
         pass
 
     @commands.command()
-    def getData(self, ctx):
+    async def getData(self, ctx):
         pass
 
 

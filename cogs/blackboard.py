b1c8a777b2d3bb407cb046f407dc29d6b77ba604
Mithul Manivannan <mithulmanimala@gmail.com>
2021-02-27 16:46:53 -0600

    Setup

--- cogs/blackboard.py
+++ cogs/blackboard.py
@@ -1,17 +1,26 @@
 import discord
+from collections import defaultdict
 from discord.ext import commands
 
 class BlackBoard(commands.Cog):
 
     def __init__(self, client):
+        self.questionFiles = defaultdict()
         self.client = client
+        self.defaultFile = None
 
     @commands.command()
-    async def addQuestion(self, ctx):
-        pass
+    async def add(self, ctx, question, answer):
+        self.questionFiles[question] = answer
+        embed = discord.Embed(title=f'Question added',
+                              description=f"Q: {question}\nA: {answer}",
+                              footer=f"Number of questions: {len(self.questionFiles)}",
+                              color=0x00aa00
+                              )
+        await ctx.send(embed = embed)
 
     @commands.command()
-    async def removeQuestion(self, ctx):
+    async def remove(self, ctx):
         pass
 
     @commands.command()

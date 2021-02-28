41a6c31be90ff60a976e909dab1767ea38b17f62
Mithul Manivannan <mithulmanimala@gmail.com>
2021-02-27 21:44:02 -0600

    Setup

--- cogs/classroom.py
+++ cogs/classroom.py
@@ -1,21 +1,21 @@
 import discord
-from collections import defaultdict
 from discord.ext import commands
 import asyncio
 import random
+
 
 class BlackBoard(commands.Cog):
 
     def __init__(self, client):
         self.questionFiles = {}
         self.client = client
-        self.MAXQ = 5
+        self.MAX_Q = 5
         self.userPoints = {}
 
     @commands.command()
     async def startGame(self, ctx):
         if len(self.userPoints) == 0 or len(self.questionFiles) == 0:
-            await ctx.send("There are no players or questions in game.")
+            await ctx.send(embed = discord.Embed(title = "There are no players or questions in game.", color = 0x00aa00))
             return
         channel = await ctx.guild.create_text_channel('classroom-game-channel')
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
@@ -26,14 +26,14 @@ class BlackBoard(commands.Cog):
             randQ, randA = random.choice(list(self.questionFiles.items()))
             del self.questionFiles[randQ]
             embed = discord.Embed(title=f"Question #{i}", description=randQ, color=0x00aa00)
-            await channel.send(embed = embed)
+            await channel.send(embed=embed)
             await asyncio.sleep(3)
             embed = discord.Embed(title=f"Times up!!", description="The answer was: " + randA, color=0x00aa00)
-            await channel.send(embed = embed)
+            await channel.send(embed=embed)
             await asyncio.sleep(1)
             embed = discord.Embed(title=f"Scores", description=self.points(ctx), color=0x00aa00)
             await asyncio.sleep(1)
-            await channel.send(embed = embed)
+            await channel.send(embed=embed)
             i += 1
         embed = discord.Embed(title=f"Game has ended.", color=0x00aa00)
         await channel.send(embed=embed)
@@ -43,7 +43,7 @@ class BlackBoard(commands.Cog):
 
     @commands.command()
     async def add(self, ctx, question, answer, *a):
-        if len(self.questionFiles) > self.MAXQ - 1:
+        if len(self.questionFiles) > self.MAX_Q - 1:
             embed = discord.Embed(title=f"Can't add anymore question.",
                                   description=f"Maximum number of questions reached",
                                   footer=f"Number of questions: {len(self.questionFiles)}",
@@ -67,7 +67,7 @@ class BlackBoard(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def remove(self, ctx, all = ""):
+    async def remove(self, ctx, all=""):
         if len(self.questionFiles) == 0:
             embed = discord.Embed(title=f'No questions to remove :slight_frown:',
                                   color=0x00aa00
@@ -84,13 +84,13 @@ class BlackBoard(commands.Cog):
                                   description="Example: !remove 5\nPlease enter the number to remove:\n" + questions,
                                   color=0x00aa00
                                   )
-            await ctx.send(embed = embed)
+            await ctx.send(embed=embed)
         elif 0 <= int(all) - 1 < len(question):
-                del self.questionFiles[question[int(all) - 1]]
-                embed = discord.Embed(title=f'Removed question at position {all}',
-                                      color=0x00aa00
-                                      )
-                await ctx.send(embed=embed)
+            del self.questionFiles[question[int(all) - 1]]
+            embed = discord.Embed(title=f'Removed question at position {all}',
+                                  color=0x00aa00
+                                  )
+            await ctx.send(embed=embed)
         else:
             embed = discord.Embed(title=f'Please enter a valid index to remove',
                                   color=0x00aa00
@@ -98,14 +98,14 @@ class BlackBoard(commands.Cog):
             await ctx.send(embed=embed)
 
     @commands.command()
-    async def showQuestions(self, ctx):
+    async def showQ(self, ctx):
         if len(self.questionFiles) == 0:
             embed = discord.Embed(title=f'There are no questions. :slight_frown:',
                                   color=0x00aa00
                                   )
         else:
             embed = discord.Embed(title=f'All questions & answers',
-                                  description=self.questionOutput(ctx),
+                                  description=self.questionOutput(),
                                   color=0x00aa00
                                   )
         await ctx.send(embed=embed)
@@ -157,11 +157,12 @@ class BlackBoard(commands.Cog):
                                   )
         await ctx.send(embed=embed)
 
-    def questionOutput(self, ctx):
+    def questionOutput(self):
         string = ""
         for k, v in self.questionFiles.items():
             string += f"Q: {k}\nA: {v}\n\n"
         return string
+
 
 def setup(client):
     client.add_cog(BlackBoard(client))
